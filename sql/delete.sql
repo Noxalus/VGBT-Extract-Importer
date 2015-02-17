7bfda26b33f2064c5e2495245586fd2f4acbb622
Noxalus <noxalus@gmail.com>
2015-02-17 23:54:17 +0100

    Update the SQL database structure file + add a new script to delete all tables in order (to avoid foreign keys constraint).

---
+++ sql/delete.sql
@@ -0,0 +1,8 @@
+DROP TABLE vgbt_extract_album_links;
+DROP TABLE vgbt_extracts;
+DROP TABLE vgbt_game_console_links;
+DROP TABLE vgbt_consoles;
+DROP TABLE vgbt_games;
+DROP TABLE vgbt_game_series;
+DROP TABLE vgbt_composers;
+DROP TABLE vgbt_albums;
